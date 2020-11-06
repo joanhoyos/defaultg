5c973a7ba24106e137b9e671f70d949a5f7ab7b8
joan sebastian hoyos <35968341+joanhoys0719@users.noreply.github.com>
2020-11-06 17:59:34 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-ScriptProcessorNode
 
-importScripts 
+const menu = document.querySelector("#mobile-menu")
+const menuLinks = document.querySelector("navbar__menu")
+
+
+menu.addEventListener("clik", function(){
+    menu.classList.toggle("is-active")
+    menuLinks.classList.toggle("active")
+}
+)
